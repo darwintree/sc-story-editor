4da263316c7439798aa7263bb86beb16856444b8
darwintree <daerwendeshu@outlook.com>
2023-07-30 22:21:48 +0800

    use default output path

--- index.ts
+++ index.ts
@@ -804,7 +804,7 @@ async function main() {
       text: "スターズ!",
     })
   );
-  story.output("F:\\Projects\\shiny-proxy\\assets\\json\\custom\\story.json");
+  story.output("./story.json");
 }
 
 main();
